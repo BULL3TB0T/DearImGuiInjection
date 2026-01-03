2c4dfba4b323b70457eb2a3a97a3c27c781c4f0b
BULLETBOT <csgodenisz@gmail.com>
2026-01-03 03:26:49 +0100

    No MelonLoader, too broken, maybe in the future

--- DearImGuiInjection.MelonIL2CPP/DearImGuiInjectionMetadata.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DearImGuiInjection.MelonIL2CPP;
-
-public sealed class DearImGuiInjectionMetadata
-{
-    public const string Author = "BULLETBOT";
-    public const string Name = "DearImGuiInjection_MelonIL2CPP";
-    public const string Version = "1.0.3";
-    public const string DownloadLink = "https://github.com/BULL3TB0T/DearImGuiInjection";
-}
--- DearImGuiInjection.MelonMono/DearImGuiInjectionMetadata.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DearImGuiInjection.MelonMono;
-
-public sealed class DearImGuiInjectionMetadata
-{
-    public const string Author = "BULLETBOT";
-    public const string Name = "DearImGuiInjection_MelonMono";
-    public const string Version = "1.0.3";
-    public const string DownloadLink = "https://github.com/BULL3TB0T/DearImGuiInjection";
-}
